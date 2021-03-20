83a182b33858bb7f8c06340e05e55c250a079790
pereira666 <antcarlospereiral@gmail.com>
2021-03-20 19:29:46 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,39 @@
+const help = (prefix) => {
+	return `
+✿ Prefix:  *「${prefix} 」*
+✿ Status: *「 Online 」*
+
+*📃 informações*
+*${prefix}info*
+*${prefix}criador*
+
+*🦋 Ações*
+*${prefix}sticker* ou *${prefix}stiker*
+*${prefix}toimg*
+*${prefix}tsticker* ou *${prefix}tstiker*
+*${prefix}tsticker seu texto aqui*\n
+*${prefix}ocr* {converter imagem em texto}
+  
+*🌊 Diversões*
+*${prefix}gtts*
+*${prefix}loli*
+*${prefix}wait*
+*${prefix}tiktokstalk*
+
+*👥 Grupo/Apenas Ademir*
+*${prefix}linkgroup*
+*${prefix}marcar*
+*${prefix}add*
+*${prefix}kick*
+*${prefix}promote*
+*${prefix}demote*
+`
+}
+
+exports.help = help
+
+
+
+
+
+
